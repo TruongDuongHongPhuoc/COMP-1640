f10c68695e0a42e8eca971f97be3dcb675a13787
TruongDuongHongPhuoc <121841902+TruongDuongHongPhuoc@users.noreply.github.com>
2024-03-06 16:59:39 +0700

    Modify Models 2

---
+++ src/main/java/com/example/comp1640/repository/FalcultyRepository.java
@@ -0,0 +1,33 @@
+package com.example.comp1640.repository;
+
+import com.example.comp1640.model.Faculty;
+import com.mongodb.client.result.UpdateResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FalcultyRepository {
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    public void updateItemQuantity(String id, String name, int group, String academicYear) {
+        Query query = new Query(Criteria.where("id").is(id));
+        Update update = new Update();
+        update.set("name",name);
+        update.set("group", group);
+        update.set("academicYear",academicYear);
+
+        UpdateResult result = mongoTemplate.updateFirst(query, update, Faculty.class);
+
+        if(result == null)
+            System.out.println("No documents updated");
+        else
+            System.out.println(result.getModifiedCount() + " document(s) updated..");
+
+    }
+}
