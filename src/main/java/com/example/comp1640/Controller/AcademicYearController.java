21ad8561f59408bae46aed56c50b8ba47fe0d18a
TruongDuongHongPhuoc <121841902+TruongDuongHongPhuoc@users.noreply.github.com>
2024-03-07 16:09:35 +0700

    Matching Git ?

--- src/main/java/com/example/comp1640/Controller/AcademicYearController.java
+++ src/main/java/com/example/comp1640/Controller/AcademicYearController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RequestMapping("/Academic")
 @Controller
 public class AcademicYearController 
 {
