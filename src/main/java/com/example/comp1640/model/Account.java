26e3f8c81a83e370dd24b309265ca81546e1bbf2
ABYDOS <106178580+MaZ-Encrypt@users.noreply.github.com>
2024-03-13 14:11:19 +0700

    Update Account.java

--- src/main/java/com/example/comp1640/model/Account.java
+++ src/main/java/com/example/comp1640/model/Account.java
@@ -19,9 +19,11 @@ public class Account {
     private String address;
     private Byte phoneNumber;
     private String password;
+    private String profileImage;
     private String roleId;
+    private String facultyId;
 
-    public Account(String id, String name, String mail, Date dateOfBirth, String address, Byte phoneNumber, String password, String roleId) {
+    public Account(String id, String name, String mail, Date dateOfBirth, String address, Byte phoneNumber, String password, String profileImage, String roleId, String facultyId) {
         this.id = id;
         this.name = name;
         this.mail = mail;
@@ -29,7 +31,9 @@ public class Account {
         this.address = address;
         this.phoneNumber = phoneNumber;
         this.password = password;
+        this.profileImage = profileImage;
         this.roleId = roleId;
+        this.facultyId = facultyId;
     }
 }
 
